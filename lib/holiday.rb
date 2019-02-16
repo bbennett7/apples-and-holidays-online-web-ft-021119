fe22df5171c2a72bbea072e6a9af9e5737151e78
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-16 07:59:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,6 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
       if holiday.to_s.include?("_")
         holiday_split = holiday.to_s.split("_")
         holiday_caps = holiday_split.each{|word| word.capitalize}
+        binding.pry
         holiday_name = holiday_caps.join(" ")
         puts "  #{holiday_name}: #{items_array.join(",")}"
       else

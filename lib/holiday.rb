56d72840a10b941371f12a846b81336129ab748e
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-16 07:53:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holiday_and_decorations.each do |holiday, items_array|
       if holiday.to_s.include?("_")
-        holiday.
+        holiday_split = holiday.split("_")
+        holiday_caps = holiday
       else
         puts "  #{holiday.capitalize}: #{items_array.join(", ")}"
       end
